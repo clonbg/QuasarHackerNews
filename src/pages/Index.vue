5ba9e38471e4e055fda89f904ca83e1581730d1a
clonbg <clonbg@gmail.com>
2020-04-20 21:35:25 +0200

    pagina principal

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,70 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <ul>
+      <q-list bordered separator v-for="historia in numerohistorias" :key="historia.id">
+      <q-item clickable v-ripple>
+      <q-item-section>{{historia.title}}</q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+      <q-item-section>
+      <q-item-label overline>Creador</q-item-label>
+      <q-item-label>{{historia.by}}</q-item-label>
+      </q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+      <q-item-section>
+      <p>{{historia.text}}</p>
+      </q-item-section>
+      </q-item>
+      <br><br><br>
+      </q-list>
+      {{numerohistorias}}
+    </ul>
   </q-page>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex",
+  data() {
+    return {
+      arrayOriginal: [],
+      indiceArray: 0,
+    }
+  },
+  methods: {
+    update() {
+      axios
+        .get("https://hacker-news.firebaseio.com/v0/beststories.json")
+        .then(response => (this.arrayOriginal = response.data))
+        .catch(function(error) {
+          // handle error
+          console.log(error);
+        })
+    }
+  },
+  computed: {
+    numerohistorias(){
+      var arrayCorto=this.arrayOriginal.slice(this.indiceArray, this.indiceArray+5)
+      var arrayDatos=[]
+      arrayCorto.forEach(element => {
+        console.log(element)
+        axios
+        .get("https://hacker-news.firebaseio.com/v0/item/"+String(element)+".json?print=pretty")
+        .then(response => (arrayDatos.push(response.data)))
+        .catch(function(error) {
+          // handle error
+          console.log(error);
+        })
+      });
+      console.log(arrayDatos)
+      return arrayDatos
+    }
+  },
+  created() {
+    this.update();
+  },
+
+};
 </script>
